6c6ee2568400e6f8341fd0fd992cae0d1f851e3f
Andrew <anluanlu123@163.com>
2016-04-26 18:13:45 +0800

    commit

--- AsyncTableViewDemo/ViewController.m
+++ AsyncTableViewDemo/ViewController.m
@@ -7,21 +7,74 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
+#import <AsyncDisplayKit/AsyncDisplayKit.h>
+#import <AsyncDisplayKit/ASAssert.h>
+@interface ViewController ()<ASTableViewDataSource, ASTableViewDelegate>
+@property (nonatomic,strong)ASTableView *tableView;
+@property (nonatomic,strong)NSMutableArray *arrayData;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+   
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+//-(id)initWithCoder:(NSCoder *)aDecoder{
+//    self=[super initWithCoder:aDecoder];
+//    if(self){
+//      [self initTableView];
+//    }
+//    return self;
+//}
+-(void)viewWillLayoutSubviews{
+
+    _tableView.frame=self.view.bounds;
 }
 
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    [self initTableView];
+    [_tableView reloadData];
+}
+
+
+-(void)initTableView{
+    if(!_tableView){
+        _tableView = [[ASTableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain asyncDataFetching:YES];
+        _tableView.asyncDataSource = self;
+        _tableView.asyncDelegate = self;
+        _tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+        [self.view addSubview:_tableView];
+    }
+   
+}
+
+#pragma mark
+#pragma mark ASTableview delegate
+
+//-(ASCellNodeBlock)tableView:(ASTableView *)tableView nodeBlockForRowAtIndexPath:(NSIndexPath *)indexPath{
+// 
+//}
+
+-(ASCellNode *)tableView:(ASTableView *)tableView nodeForRowAtIndexPath:(NSIndexPath *)indexPath{
+    ASTextCellNode *textCellnode=[ASTextCellNode new];
+    textCellnode.text=indexPath.description;
+    return textCellnode;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 10;
+}
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 100;
+}
 @end
+
+
+
+
+
+
